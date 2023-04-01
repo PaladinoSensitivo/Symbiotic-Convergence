34a17e2a5cec16f2c73e33f328db37889d5b4284
danielsantosds7 <danielsantosds7@gmail.com>
2023-04-01 19:09:42 -0300

    implementado mecanica basica + navmesh

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent enemy;
+    private Transform point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        point = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(point.position);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    Rigidbody Rig;
+
+    void Start()
+    {
+        Rig = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 Position = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Rig.velocity = Position.normalized * Speed;
+    }
+}
