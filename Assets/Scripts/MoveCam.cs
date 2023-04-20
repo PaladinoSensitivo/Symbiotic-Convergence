199656ab87c04b7416750f21f5d72973b0e936c7
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2023-04-20 20:14:26 -0300

    pequenos ajustes2

--- Assets/Scripts/MoveCam.cs
+++ Assets/Scripts/MoveCam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCAM : MonoBehaviour
+public class MoveCam : MonoBehaviour
 {
     [SerializeField]
     private Vector3 offset;
